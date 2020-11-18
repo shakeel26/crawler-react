3a483097ef0030d63370e9c96b01dcab3db1dd01
Shakeel Ahmed <shakeel.ahmed@sti2.at>
2020-11-18 02:09:12 +0100

    Add HomePage content on frontend from /getAllCourses

---
+++ src/components/Details.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import {BrowserRouter as Router, Link, Route, Switch, useParams} from "react-router-dom";
+
+// const Details = (props) =>{
+//
+// }
+// export default Details;
+
+function Details(props) {
+    // We can use the `useParams` hook here to access
+    // the dynamic pieces of the URL.
+    let {degreeName} = useParams();
+    return (
+        <div>
+            <h3>ID: {degreeName}</h3>
+            <p> {props.singleCourse.degreeType}</p>
+        </div>
+    );
+}
+
+export default Details;
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import Details from "./Details";
+
+
+const Home = (props) => {
+    const [degreeName, setDegreeName] = useState("");
+    const [oneCourse, setOneCourse] = useState([]);
+
+
+    return <Router>
+        <div>
+            {props.allCourses.map((eachCourse, index) => <div key={index}>
+                <div id={"deptName-" + index}><h5>{eachCourse.deptName}</h5><span></span></div>
+                <Link to={"/" + degreeName} name={eachCourse.crawledCourses.degreeName} onClick={(e) => {
+//                        e.preventDefault();
+                    setOneCourse(eachCourse);
+                    setDegreeName(eachCourse.crawledCourses.degreeName.replace(/ /g, ''));
+                }}> {eachCourse.crawledCourses.degreeName} </Link>
+                <div id={"degreeType-" + index}><span>{eachCourse.degreeType}</span></div>
+                <div id={"language-" + index}><span>{eachCourse.crawledCourses.language}</span></div>
+                <div id={"uniName-" + index}><a href={eachCourse.crawledCourses.website} rel="link to uni site"
+                                                target="_blank"> {eachCourse.crawledCourses.uniName}</a><span></span>
+                </div>
+            </div>)}
+            <Switch>
+                <Route path="/:degreeName" children={<Details singleCourse={oneCourse}/>}/>
+            </Switch>
+        </div>
+    </Router>
+}
+
+export default Home;
